17793de3fcaa27c42913ba31a7866cd13b31ef3a
KamizatoR <112645912+kamizatoR@users.noreply.github.com>
2022-09-25 14:02:52 +0000

    [Stop]4-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,37 @@
 class BooksController < ApplicationController
   def index
+    @book_new = Book.new
+    @user = current_user
+    @books =Book.all
+
   end
 
   def show
+    @book_new = Book.new
+
+    @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def edit
+
   end
-  
-  def new 
-    
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      redirect_to root_path
+    end
+
   end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :opinion)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,28 @@ class UsersController < ApplicationController
 
   def edit
   end
-  
+
   def show
+    @book_new = Book.new
+    
+    @user = User.find(params[:id])
+    @books = @user.books
   end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      redirect_to root_path
+    end
+  end
+  
+  private 
+  
+   def book_params
+      params.require(:book).permit(:title, :opinion)
+   end
+  
 end

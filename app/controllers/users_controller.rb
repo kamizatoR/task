bf2bc19f2f1bdc3f19cf5b4795941beeba39bf67
KamizatoR <112645912+kamizatoR@users.noreply.github.com>
2022-09-27 14:35:17 +0000

    [Stop]4-25

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,19 +3,26 @@ class UsersController < ApplicationController
     @book_new = Book.new
     @user = current_user
     @users = User.all
-    
+
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
 
   def show
     @book_new = Book.new
-    
+
     @user = User.find(params[:id])
     @books = @user.books
   end
-  
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
@@ -25,11 +32,15 @@ class UsersController < ApplicationController
       redirect_to root_path
     end
   end
-  
-  private 
-  
+
+  private
+
    def book_params
       params.require(:book).permit(:title, :opinion)
    end
-  
+
+   def user_params
+     params.require(:user).permit(:name, :introduction, :profile_image)
+   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get "homes/about" => "homes#about",as: "about"
-  resources :users, only: [:edit,  :index, :show ]
+  resources :users, only: [:edit,  :index, :show, :update ]
   resources :books, only: [:edit, :show, :index, :create, :destroy, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
